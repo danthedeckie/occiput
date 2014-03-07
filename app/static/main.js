302e21a6b35ca62291aac9f613dc8a1363d7d24e
Daniel Fairhead <daniel.fairhead@om.org>
2014-03-07 12:33:39 +0000

    basic cpuload type svg widget... numbers not accurate yet.

--- app/static/main.js
+++ app/static/main.js
@@ -1,15 +1,25 @@
 function start_getting(sections) {
-    var i;
+    var i,
+        again = function(){start_getting(sections);};
 
     for (i=0; i<sections.length; i++) {
         $.get('/script/' + sections[i], function(s){ return function(data) {
-            var type = $('#content_' + s).data('type');
+            var type = $('#content_' + s).data('type'),
+                new_html = '';
+
             switch (type) {
+                case 'svg':
+                    //new_html = '<svg width="200px" height="200px">' + data['data'] + '</svg>';
+                    new_html = data['data'];
+                    break;
                 case 'pretext':
                 default:
-                    $('#content_' + s).html('<pre>' + data['data'] + '</pre>');
+                    new_html = '<pre>' + data['data'] + '</pre>';
             }
+            $('#content_' + s).html(new_html);
+
         }}(sections[i]));
     }
+    setTimeout(again, 4000);
 
 }
